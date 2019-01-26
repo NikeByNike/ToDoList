600959c162cd778046f9f529ec38a02cdae957cb
NikeByNike <workwithme@rambler.ru>
2019-01-26 21:24:44 +0300

    fix(FormList) space

--- src/ToDoList/FormList.js
+++ src/ToDoList/FormList.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
 class FormList extends Component {
 	
 	listItem(e) {
 		e.preventDefault();
-		if (this.inputTitle.value.trim() !== '') {
+		if (this.inputTitle.value.trim() !== "") {
 			const ListItem = {
 				id: Date.now(),
 				title: this.inputTitle.value.trim(),
@@ -13,38 +13,41 @@ class FormList extends Component {
 				cat_id: parseInt(this.selectOption.value, 10)
 			};
 			this.props.addListItem(ListItem);
-			this.inputTitle.value = '';
-			this.inputText.value = '';
+			this.inputTitle.value = "";
+			this.inputText.value = "";
 		}
 	}
 
 	render() {
-	  return (
-			<div className="toDoList_form">
-				<form autoComplete="off" onSubmit={this.listItem.bind(this)}>
-					<div>
-						<input ref={(title) => { this.inputTitle = title }} type="text" 
-									 placeholder="Title" />
-					</div>
-					<div>
-						<textarea ref={(text) => { this.inputText = text }} 
-											cols="40" rows="2" placeholder="Text" />
-					</div>
-					<div>
-						<label htmlFor="select-choice">Выберите категорию:</label><br/>
-						<select ref={(category) => { this.selectOption = category }} 
-										id="select-choice">
-							{this.props.menu.map(item => (
-								<option key={item.id} value={item.id}>{item.name}</option>
-							))}
-						</select>
-					</div>
-					<div>
-						<input type="submit" value="Добавить" />
-					</div>
-				</form>
-			</div>
-	  );
+		return (
+			<>
+				<div>All</div>
+				<div className="toDoList_form">
+					<form autoComplete="off" onSubmit={this.listItem.bind(this)}>
+						<div>
+							<input ref={(title) => { this.inputTitle = title }} type="text"
+										 placeholder="Title" />
+						</div>
+						<div>
+							<textarea ref={(text) => { this.inputText = text }}
+												cols="40" rows="2" placeholder="Text" />
+						</div>
+						<div>
+							<label htmlFor="select-choice">Выберите категорию:</label><br/>
+							<select ref={(category) => { this.selectOption = category }}
+											id="select-choice">
+								{this.props.menu.map(item => (
+									<option key={item.id} value={item.id}>{item.name}</option>
+								))}
+							</select>
+						</div>
+						<div>
+							<input type="submit" value="Добавить" />
+						</div>
+					</form>
+				</div>
+			</>
+		);
 	}
 }
 
